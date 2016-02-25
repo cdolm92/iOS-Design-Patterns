582c78195f2a2dd9fea339f22afade64cd03144f
cdolm92 <cdolm92@gmail.com>
2016-02-25 01:02:15 -0500

    first commit

---
+++ Alloc_Init/Alloc_Init/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Alloc_Init
+//
+//  Created by Christella on 2/25/16.
+//  Copyright © 2016 Christella. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
